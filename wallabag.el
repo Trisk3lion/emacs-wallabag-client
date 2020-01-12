f8c54b0039696eff9796e8402ee472f0a562c21b
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-01-11 21:52:17 -0500

    Initial commit

---
+++ wallabag.el
@@ -0,0 +1,30 @@
+;;; wallabag.el --- An Emacs Wallabag client         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Jeremy Dormitzer
+
+;; Author: Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
+;; Keywords: tools, multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is an Emacs client for the Wallabag read-later service.
+
+;;; Code:
+
+
+
+(provide 'wallabag)
+;;; wallabag.el ends here
