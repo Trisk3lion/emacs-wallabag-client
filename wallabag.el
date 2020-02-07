0fb146235fcf982fe69a517937285a4545181485
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-02-07 13:58:09 -0500

    Implement create, read, and delete operations

--- wallabag.el
+++ wallabag.el
@@ -23,23 +23,123 @@
 ;; This is an Emacs client for the Wallabag read-later service.
 
 ;;; Code:
+(defgroup wallabag nil
+  "Customization options for the Emacs Wallabag client")
 
-(defun wallabag-url (base-url path)
+(defcustom wallabag-base-url nil
+  "The base URL of the Wallabag instance"
+  :group 'wallabag
+  :type '(string))
+
+(defcustom wallabag-client-id nil
+  "The client ID of the Wallabag client"
+  :group 'wallabag
+  :type '(string))
+
+(defcustom wallabag-client-secret nil
+  "The client secret of the Wallabag client"
+  :group 'wallabag
+  :type '(string))
+
+(defcustom wallabag-username nil
+  "The username of the Wallabag user"
+  :group 'wallabag
+  :type '(string))
+
+(defcustom wallabag-password nil
+  "The password of the Wallabag user"
+  :group 'wallabag
+  :type '(string))
+
+(defvar wallabag-cached-auth nil
+  "Cached Wallabag authentication data")
+
+(cl-defun wallabag-validate-connection-params ()
+  "Validates that all necessary connection parameter
+are defined. Raises an error if validation fails."
+  (let ((do-validate (lambda (sym)
+		       (when (or (not (boundp sym))
+				 (not (symbol-value sym)))
+			 (error "Unable to connect to Wallabag. Please set %s" sym)))))
+    (funcall do-validate 'wallabag-base-url)
+    (funcall do-validate 'wallabag-client-id)
+    (funcall do-validate 'wallabag-client-secret)
+    (funcall do-validate 'wallabag-username)
+    (funcall do-validate 'wallabag-password)))
+
+(cl-defun wallabag-url (base-url path)
   (format "%s/%s"
-    (s-chop-suffix "/" base-url)
-    (s-chop-prefix "/" path)))
+	  (s-chop-suffix "/" base-url)
+	  (s-chop-prefix "/" path)))
 
-(defun wallabag-get-api-token (base-url client-id client-secret username password)
-  "Obtains a new Wallabag API access token."
-  (request
-    (wallabag-url base-url "oauth/v2/token")
-    :type "POST"
-    :parser 'json-read
-    :data `(("grant_type" . "password")
-            ("client_id" . ,client-id)
-            ("client_secret" . ,client-secret)
-            ("username" . ,username)
-            ("password" . ,password))))
+(cl-defun wallabag-get-auth-data (&optional callback)
+  "Obtains a new Wallabag API access token and refresh token."
+  (wallabag-validate-connection-params)
+  (let ((response (request
+		    (wallabag-url wallabag-base-url "oauth/v2/token")
+		    :sync (not callback)
+		    :complete callback
+		    :type "POST"
+		    :parser 'json-read
+		    :data `(("grant_type" . "password")
+			    ("client_id" . ,wallabag-client-id)
+			    ("client_secret" . ,wallabag-client-secret)
+			    ("username" . ,wallabag-username)
+			    ("password" . ,wallabag-password)))))
+    (when (not callback) response)))
+
+;; TODO support reauthentication when the access token expires
+(cl-defun wallabag-request (endpoint &key method callback form-data parser params)
+  "Makes a request to a Wallabag instance,
+authenticating if necessary"
+  (let ((make-request (cl-function
+		       (lambda (&key data &allow-other-keys)
+			 (let* ((access-token (alist-get 'access_token
+							 data))
+				(response (request
+					    (wallabag-url wallabag-base-url endpoint)
+					    :sync (not callback)
+					    :type (or method "GET")
+					    :headers `(("Authorization" .
+							,(format "Bearer %s" access-token)))
+					    :data form-data
+					    :params params
+					    :complete callback
+					    :parser (or parser 'json-read))))
+			   (when (not callback) response))))))
+    (if callback
+	(if wallabag-cached-auth
+	    (funcall make-request :data wallabag-cached-auth)
+	  (wallabag-get-auth-data (cl-function
+				   (lambda (response)
+				     (setq wallabag-cached-auth (request-response-data response))
+				     (funcall make-request :data wallabag-cached-auth)))))
+      (when (not wallabag-cached-auth)
+	(setq wallabag-cached-auth (request-response-data
+				    (wallabag-get-auth-data))))
+      (funcall make-request :data wallabag-cached-auth))))
+
+;; TODO handle paging (page size is 30 by default)
+(cl-defun wallabag-entries (&optional callback)
+  "Retrieves the list of Wallabag entries"
+  (let ((response (wallabag-request "/api/entries.json"
+				    :callback callback)))
+    (when (not callback) response)))
+
+(cl-defun wallabag-add-entry (url &optional callback)
+  "Adds a new Wallabag entry"
+  (let ((response (wallabag-request "/api/entries.json"
+				    :method "POST"
+				    :form-data `(("url" . ,url))
+				    :callback callback)))
+    (when (not callback) response)))
+
+(cl-defun wallabag-delete-entry (id &optional callback)
+  "Deletes a Wallabag entry"
+  (let ((response (wallabag-request (format "/api/entries/%s.json" id)
+				    :method "DELETE"
+				    :callback callback)))
+    (when (not callback) response)))
 
 (provide 'wallabag)
 ;;; wallabag.el ends here
